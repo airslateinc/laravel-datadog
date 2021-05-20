83bc20ccf27f01d29487c2a3f190edcefd9c645f
Oleksii Bilotserkivskyi <dr2fairy@gmail.com>
2021-05-20 10:31:52 +0300

    [AA-4244] Catch statsd errors (#40)

--- src/Services/Datadog.php
+++ src/Services/Datadog.php
@@ -6,11 +6,27 @@ namespace AirSlate\Datadog\Services;
 
 use DataDog\DogStatsd;
 use Illuminate\Support\Facades\Config;
+use Psr\Log\LoggerInterface;
+use Throwable;
 
 class Datadog extends DogStatsd
 {
     /** @var array<string, string> */
     private $tags = [];
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * Datadog constructor.
+     * @param mixed[] $config
+     */
+    public function __construct(array $config = [])
+    {
+        parent::__construct($config);
+        $this->logger = app('log');
+    }
 
     /**
      * @inheritDoc
@@ -20,7 +36,14 @@ class Datadog extends DogStatsd
     {
         $tags = $this->prepareTags(is_array($tags) ? $tags : null);
 
-        parent::send($data, $sampleRate, $tags);
+        try {
+            parent::send($data, $sampleRate, $tags);
+        } catch (Throwable $exception) {
+            $this->logger->error(
+                'Could not send data to Datadog Statsd',
+                ['exception' => $exception]
+            );
+        }
     }
 
     /**
@@ -29,10 +52,17 @@ class Datadog extends DogStatsd
      */
     public function timing($stat, $time, $sampleRate = 1.0, $tags = null): void
     {
-        parent::timing($stat, $time, $sampleRate, $tags);
+        try {
+            parent::timing($stat, $time, $sampleRate, $tags);
 
-        if (Config::get('datadog.is_send_increment_metric_with_timing_metric') !== false) {
-            $this->increment($stat, $sampleRate, $tags);
+            if (Config::get('datadog.is_send_increment_metric_with_timing_metric') !== false) {
+                $this->increment($stat, $sampleRate, $tags);
+            }
+        } catch (Throwable $exception) {
+            $this->logger->error(
+                'Could not send timing data to Datadog Statsd',
+                ['exception' => $exception]
+            );
         }
     }
 
